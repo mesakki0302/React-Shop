5c9c09b74e01d24643e1becb5c54d02939a0c8fb
mesakki0302 <mesakki0302@gmail.com>
2026-03-22 15:28:37 +0530

    new page contact

--- src/Components/ContactPage/Contact.jsx
+++ src/Components/ContactPage/Contact.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import'./Contact.css'
 
 function Contact() {
   return (
-    <div>Contact</div>
+   <div className="contact-container">
+      
+      <div className="contact-box">
+        <h2>Contact Us</h2>
+        <p>We’d love to hear from you! Fill out the form below.</p>
+
+        <form className="contact-form">
+          <input type="text" placeholder="Your Name" required />
+          <input type="email" placeholder="Your Email" required />
+          <input type="text" placeholder="Subject" required />
+          
+          <textarea placeholder="Your Message" rows="5" required></textarea>
+
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
+
+    </div>
   )
 }
 
